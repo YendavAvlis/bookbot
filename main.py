da07db937777fa43fc59e93eaa75e9f20081094e
YendavAvlis <vadneysilva@hotmail.com>
2024-11-17 08:17:37 +0200

    Count character feature added

--- main.py
+++ main.py
@@ -8,4 +8,22 @@ def count_words(book):
         count += 1
     return f"The book contains {count} words!"
 
+def count_letters(book):
+    count_letters = {}
+
+    for word in book:
+        words = word.split()
+        for letter in words:
+            characters = letter.lower()
+            if characters.isalpha():
+                if characters in count_letters:
+                    count_letters[characters] += 1
+                else:
+                    count_letters[characters] = 1
+    return count_letters
+
 print(count_words(content))
+print(count_letters(content))
+
+# print(count_letters)
+# print(dir(count_letters))
